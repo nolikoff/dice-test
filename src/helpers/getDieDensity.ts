eb8cb8b8ca139ee39e65260e6ac2206fa5f8a5e5
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-05 01:08:06 +0300

    Update getDieDensity.ts

--- src/helpers/getDieDensity.ts
+++ src/helpers/getDieDensity.ts
@@ -2,11 +2,14 @@ import { Die } from "../types/Die";
 
 /** Get the density multiplier for a die */
 export function getDieDensity(die: Die): number {
-  if (die.style === "IRON") {
-    return 2;
-  } else if (die.style === "WALNUT" || die.style === "GLASS") {
-    return 1.5;
-  } else {
-    return 1;
-  }
+  
+  // if (die.style === "IRON") {
+  //   return 2;
+  // } else if (die.style === "WALNUT" || die.style === "GLASS") {
+  //   return 1.5;
+  // } else {
+  //   return 1;
+  // }
+
+  return 1;
 }
